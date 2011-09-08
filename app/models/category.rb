52e7ab99ad7a8d1d89f5a53ffb191e1e0a14e7d7
Kamal Ejaz <kamal.ejaz@nxb.com.pk>
2011-09-08 15:04:03 +0500

    Category bug fix paperclip

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
     has_attached_file :display,
-      styles:{ medium: "300x300>", thumb: "100x100>" }
+      styles:{ medium: "300x300>", thumb: "100x100" },
+      default_url: '/images/folder.png'
     def to_s
         self.name
     end
