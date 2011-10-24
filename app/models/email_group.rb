ea95fcc83f83546449a02b669dc6de3c04335d2b
Imran Latif <imran@nx00205.(none)>
2011-10-24 18:40:09 +0500

    Committing Code.

--- app/models/email_group.rb
+++ app/models/email_group.rb
@@ -1,7 +1,7 @@
 class EmailGroup < ActiveRecord::Base
 
   validates   :name, :presence => true, :uniqueness => true
-  validates   :description, :presence => true
+#  validates   :description, :presence => true
   
   has_and_belongs_to_many   :email_accounts
 
